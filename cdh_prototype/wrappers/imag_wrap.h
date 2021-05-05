0eec39bdc34f1b50bdb6cc924c0145c538f7a04e
lachlanstewart <lachlan.rocks.em@gmail.com>
2021-05-05 12:31:45 -0700

    Developed getImage() and getPackage() functions. Untested

--- cdh_prototype/wrappers/imag_wrap.h
+++ cdh_prototype/wrappers/imag_wrap.h
@@ -15,11 +15,9 @@ static const uint8_t GET_PICTURE_SIZE   = 0x03;
 static const uint8_t GET_THUMBNAIL      = 0x04;
 static const uint8_t GET_PICTURE        = 0x05;
 static const uint8_t SET_CONTRAST       = 0x06;
-static const uint8_t SET_BRIGTHNESS     = 0x07;
+static const uint8_t SET_BRIGHTNESS     = 0x07;
 static const uint8_t SET_EXPOSURE       = 0x08;
 static const uint8_t SET_SLEEP_TIME     = 0x09;
-
-
 
 void testCheckStatus(char parameter);
 
