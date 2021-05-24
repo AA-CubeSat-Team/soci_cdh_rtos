fe8d2439d748260a3cf3eb2f4069b2d4e5256382
lachlanstewart <lachlan.rocks.em@gmail.com>
2021-05-24 12:38:10 -0700

    Deleted getThumbnail and getThumbnailSize

--- cdh_prototype/wrappers/imag_wrap.h
+++ cdh_prototype/wrappers/imag_wrap.h
@@ -10,9 +10,9 @@
 /* Commands that can be tested */
 static const uint8_t CHECK_STATUS       = 0x00;
 static const uint8_t TAKE_PICTURE       = 0x01;
-static const uint8_t GET_THUMBNAIL_SIZE = 0x02;
+// static const uint8_t GET_THUMBNAIL_SIZE = 0x02; IMG not using these commands anymore.
 static const uint8_t GET_PICTURE_SIZE   = 0x03;
-static const uint8_t GET_THUMBNAIL      = 0x04;
+// static const uint8_t GET_THUMBNAIL      = 0x04; IMG not using these commands anymore.
 static const uint8_t GET_PICTURE        = 0x05;
 static const uint8_t SET_CONTRAST       = 0x06;
 static const uint8_t SET_BRIGHTNESS     = 0x07;
