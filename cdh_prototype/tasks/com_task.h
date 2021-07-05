197daa6de48d66c5c91e8586f1ebfa8bbb403515
Harshal Patil <you@example.com>
2021-07-04 17:31:36 -0700

    UART write and Read APIs for com and imag tasks

--- cdh_prototype/tasks/com_task.h
+++ cdh_prototype/tasks/com_task.h
@@ -10,8 +10,25 @@
 #include "fsl_device_registers.h"
 #include "fsl_debug_console.h"
 #include "board.h"
-
 #include "clock_config.h"
+
+/*UART includes*/
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "fsl_lpuart.h"
+
+/*******************************************************************************
+ * Definitions for UART 1
+ ******************************************************************************/
+#define LPUART_1           	LPUART1
+#define LPUART1_CLK_FREQ   	BOARD_DebugConsoleSrcFreq()
+#define UART1_IRQn       	LPUART1_IRQn
+#define UART1_IRQHandler 	LPUART1_IRQHandler
+
+/*! @brief Ring buffer size for UART 1 (Unit: Byte). */
+#define UART1_RING_BUFFER_SIZE 16
+
+
 
 /*******************************************************************************
  * Variable Declarations
@@ -29,4 +46,7 @@ extern TaskHandle_t TaskHandler_com;
 /*******************************************************************************
  * Function declarations
  ******************************************************************************/
+void UART1_IRQHandler(void);
 void com_task(void *pvParameters);
+
+
