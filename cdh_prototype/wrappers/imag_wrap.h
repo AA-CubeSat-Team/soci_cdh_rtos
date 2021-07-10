78af302e136909df24cee70dfdb968d73d119753
lachlanstewart <lachlan.rocks.em@gmail.com>
2021-07-10 17:17:26 -0400

    Changed static constants to macros in header file to support use in imag_task

--- cdh_prototype/wrappers/imag_wrap.h
+++ cdh_prototype/wrappers/imag_wrap.h
@@ -1,3 +1,4 @@
+#include <stdint.h>
 /*
  *
  * GPIO_AD_B1_10	UART4_TX
@@ -8,17 +9,17 @@
 
 
 /* Commands that can be tested */
-static const uint8_t CHECK_STATUS       = 0x00;
-static const uint8_t TAKE_PICTURE       = 0x01;
-// static const uint8_t GET_THUMBNAIL_SIZE = 0x02; IMG not using these commands anymore.
-static const uint8_t GET_PICTURE_SIZE   = 0x03;
-// static const uint8_t GET_THUMBNAIL      = 0x04; IMG not using these commands anymore.
-static const uint8_t GET_PICTURE        = 0x05;
-static const uint8_t SET_CONTRAST       = 0x06;
-static const uint8_t SET_BRIGHTNESS     = 0x07;
-static const uint8_t SET_EXPOSURE       = 0x08;
-static const uint8_t SET_SLEEP_TIME     = 0x09;
-static const uint8_t PADDING            = 0xAA;
+#define CHECK_STATUS     0x00
+#define TAKE_PICTURE     0x01
+// #define GET_THUMBNAIL_SIZE 0x02 IMG not using these commands anymore.
+#define GET_PICTURE_SIZE 0x03
+// #define GET_THUMBNAIL 0x04 IMG not using these commands anymore.
+#define GET_PICTURE      0x05
+#define SET_CONTRAST     0x06
+#define SET_BRIGHTNESS   0x07
+#define SET_EXPOSURE     0x08
+#define SET_SLEEP_TIME   0x09
+#define PADDING          0xAA
 
 void testCheckStatus(char parameter);
 
