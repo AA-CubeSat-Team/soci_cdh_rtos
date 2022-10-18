cb665aa4976af67d24e23c851c22f52f9266a3c6
andyliu8818 <115109494+andyliu8818@users.noreply.github.com>
2022-10-18 16:39:48 -0700

    Delete gnc_task.h

--- cdh_prototype/gnc_task.h
+++
@@ -1,7 +0,0 @@
-#define GNC_ENABLE	0
-
-// calculates values based on sensor values and output data for actuators
-// run this function every 250 ms to make GNC build code real-time
-void rt_OneStep(void);
-
-void gnc_task(void *pvParameters);
