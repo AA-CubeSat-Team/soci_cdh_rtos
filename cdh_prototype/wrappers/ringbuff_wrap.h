2568edfa83fcc48cfa6e3b8384c473597ceca8fc
Mazukm <105970409+Mazukm@users.noreply.github.com>
2023-01-09 22:46:16 -0800

    Update ringbuff_wrap.h

--- cdh_prototype/wrappers/ringbuff_wrap.h
+++ cdh_prototype/wrappers/ringbuff_wrap.h
@@ -1,17 +1,31 @@
-typedef struct circular_buf_t circular_buf_t;
+//
+// Created by mikhail Mazukabzov on 1/9/23.
+//
 
-typedef circular_buf_t* cbuf_handle_t;
+#ifndef UNTITLED2_RING_BUFF_WRAP_H
+#define UNTITLED2_RING_BUFF_WRAP_H
+#pragma once
 
-typedef stored_data stored_data;
+#include <stdint.h>
 
-#define amount size;
+#define stored_data int
+#define Size 100
 
-// The hidden definition of our circular buffer structure
-struct circular_buf_t {
-	stored_data buffer[size];
-	uint head = 0;
-	uint tail = 0;
-	uint max_used = 0;
-	uint max = size;
-	char full = 0;
-};
+typedef struct Buffer {
+    stored_data data[Size];
+    long upper_bound;
+    long read_index;
+    long write_index;
+    short full_flag;
+} Buffer;
+
+Buffer *initBuffer();
+
+void insert(Buffer *buffer, stored_data given_data);
+
+stored_data read(Buffer *buffer);
+
+void refreshBuffer(Buffer *buffer);
+
+//#include "ringbuff.c"
+#endif //UNTITLED2_RING_BUFF_WRAP_H
